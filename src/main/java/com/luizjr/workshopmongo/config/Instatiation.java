fac8532e5c92367673d15e4c45cea4d0ac86bcaf
luizjrandrade <jr.luizandrade@gmail.com>
2021-12-27 23:13:38 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/luizjr/workshopmongo/config/Instatiation.java
+++ src/main/java/com/luizjr/workshopmongo/config/Instatiation.java
@@ -1,13 +1,17 @@
 package com.luizjr.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.luizjr.workshopmongo.domain.Post;
 import com.luizjr.workshopmongo.domain.User;
 import com.luizjr.workshopmongo.repository.UserRepository;
+import com.luizjr.workshopmongo.repository.PostRepository;
 
 @Configuration
 public class Instatiation implements CommandLineRunner {
@@ -15,18 +19,28 @@ public class Instatiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User luiz = new User(null, "Luiz Junior", "luizjr@email.com");
 		User lara = new User(null, "Lara Fernanda", "doce@email.com");
 		User filipe = new User(null, "Filipe Gabriel", "pingo@email.com");
 		User marly = new User(null, "Marly Siqueira", "cherin@email.com");
 		
-		userRepository.saveAll(Arrays.asList(luiz, lara, filipe, marly));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para Palmas. Té Logo!", marly);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", marly);
 		
+		userRepository.saveAll(Arrays.asList(luiz, lara, filipe, marly));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
