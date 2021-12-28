db1e806c1f8fa48de27e8d5d73d7bd4170c39ba6
luizjrandrade <jr.luizandrade@gmail.com>
2021-12-27 21:56:20 -0300

    Atualização de usuário com PUT

--- src/main/java/com/luizjr/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luizjr/workshopmongo/resources/UserResource.java
@@ -51,5 +51,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();//retorna código 204
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 }
