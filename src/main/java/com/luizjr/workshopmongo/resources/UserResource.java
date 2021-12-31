de5a2d5e01406ff56a52016ebd5ce1617ad0e57a
luizjrandrade <jr.luizandrade@gmail.com>
2021-12-31 15:46:36 -0300

    Buscas posts usuário

--- src/main/java/com/luizjr/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luizjr/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luizjr.workshopmongo.domain.Post;
 import com.luizjr.workshopmongo.domain.User;
 import com.luizjr.workshopmongo.dto.UserDTO;
 import com.luizjr.workshopmongo.service.UserService;
@@ -59,5 +60,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET) //busca os posts do usuário
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 }
